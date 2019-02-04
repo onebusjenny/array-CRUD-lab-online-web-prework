303f29eaf457d153508cfc7b183e7039f4492667
Jenny Chu <jchudesign0930@gmail.com>
2019-02-04 08:37:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,17 +7,14 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = [3,6,2,7]
   array.pop
 end
 
 def add_element_to_start_of_array(array, element)
-  array = [3,6,2,7]
   array.unshift
 end
 
 def remove_element_from_end_of_array(array)
-  array = [3,6,2,7]
   array.pop
 end
 
